99a1301c840fa1ac67867217ee30f56d4915bc7b
Maksim Strelchenko <maksim.strelchenko99@gmail.com>
2023-11-26 10:31:45 +0300

    add navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,17 @@
 import Navbar from './components/Navbar';
 import {Route, BrowserRouter as Router, Routes} from 'react-router-dom';
 
+import { Home, About, Contacts } from './pages'
+
 const App = () => {
   return (
     <main className='bg-slate-300/20'>
       <Router>
         <Navbar />
         <Routes>
-          <Route path="/" element={'Home'} />
-          <Route path="/about" element={'About'} />
-          <Route path="/contact" element={'Concats'} />
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contacts" element={<Contacts />} />
         </Routes>
       </Router>
     </main>
