c79547da3a38cbb1d200b24d0f5488c188ae8a2b
Maksim Strelchenko <maksim.strelchenko99@gmail.com>
2023-11-27 19:31:31 +0300

    made 3d keyboard relative to user grabbing, add background text

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,18 +3,17 @@ import { NavLink } from 'react-router-dom'
 const Navbar = () => {
   return (
     <header className="header">
-      <NavLink to="/" className="w-10 h-10 rounded-lg bg-black items-center justify-center flex font-bold shadow-sm shadow-white">
-        <p className="text-white">KB</p>
+      <NavLink to="/" className="w-10 h-10 rounded-lg bg-neutral-800 items-center justify-center flex font-bold hover:shadow-md">
+        <p className="text-white">MI</p>
       </NavLink>
       <nav className="flex text-lg gap-7 font-medium">
-        <NavLink to="/about" className={({ isActive }) => isActive ? "text-white hover:underline" : "text-fuchsia-400 hover:underline"}>
+        <NavLink to="/about" className={({ isActive }) => isActive ? "text-white hover:underline" : "text-purple-300 hover:underline"}>
           About
         </NavLink>
-        <NavLink to="/contacts" className={({ isActive }) => isActive ? "text-white hover:underline" : "text-fuchsia-400 hover:underline"}>
+        <NavLink to="/contacts" className={({ isActive }) => isActive ? "text-white hover:underline" : "text-purple-300 hover:underline"}>
           Contacts
         </NavLink>
       </nav>
-
     </header>
   )
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,24 +1,49 @@
 import { Canvas } from '@react-three/fiber'
-import { Suspense } from 'react'
+import { useState, Suspense } from 'react'
 import Loader from '../components/Loader'
 
 import Keyboard from '../models/Keyboard';
 
 const Home = () => {
+  const [isRotating, setIsRotating] = useState(false);
+
+  const adjustKeyboardForScreenSize = () => {
+    let screenScale = null;
+    let screenPosition = [0, 0, -30];
+    let rotation = [0, 6.25, -0.01];
+
+    if (window.innerWidth < 768) {
+      screenScale = [0.9, 0.9, 0.9];
+    } else {
+      screenScale = [1, 1, 1];
+    }
+
+
+    return [screenScale, screenPosition, rotation]
+  }
+
+  const [keyboardScale, keyboardPosition, keyboardRotation] = adjustKeyboardForScreenSize();
+
   return (
-    <section className="w-full h-screen bg-black relative">
+    <section className="w-full h-screen bg-neutral-900 relative">
+        <h1 className="leading-none absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-slate-50 font-bold text-[200px]">MECHANICAL <br/> INDUSTRY</h1>
+        
         <Canvas 
-            className='w-full h-screen bg-transparent'
+            className={`w-full h-screen bg-transparent ${isRotating ? 'cursor-grabbing' : 'cursor-grab'}`}
             camera={{ near: 0.1, far: 1000}}
         >
             <Suspense fallback= {<Loader />}>
-                <directionalLight />
-                <ambientLight />
-                <pointLight />
-                <spotLight />
-                <hemisphereLight />
+                <directionalLight position={[100, 1, 1]} intensity={2}/>
+                <ambientLight intensity={0.5}/>
+                <hemisphereLight skyColor="#b1e1ff" groundColor="#000000" intensity={1}/>
 
-                <Keyboard />
+                <Keyboard 
+                  position={keyboardPosition}
+                  scale={keyboardScale}
+                  rotation={keyboardRotation}
+                  isRotating={isRotating}
+                  setIsRotating={setIsRotating}
+                />
             </Suspense>
         </Canvas>
     </section>
